93fa4de9c2c8afc3997d4fb2fd9937c60965a0cf
YohannMaiVan <yohanm888@gmail.com>
2023-09-11 13:32:22 +0200

    💡 F02: main > Delete createBrowserRouter comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-//   {
-//     path: "/order",
-//     element: <OrderPage nouveauNom={nouveauNom} />,
-//   },
-// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
