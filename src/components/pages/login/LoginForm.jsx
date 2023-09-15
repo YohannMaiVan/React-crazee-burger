896e3391781ba8b4480ea50412d9a765d674a2cd
YohannMaiVan <yohanm888@gmail.com>
2023-09-15 16:44:11 +0200

    💄 F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
 
 export default function LoginForm() {
   //state
@@ -20,23 +21,73 @@ export default function LoginForm() {
   //affichage
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous</h1>
-      <br />
+      <h1>Bienvenue chez nous !</h1>
+      <hr />
       <h2>Connectez-vous</h2>
-      <label>
+      <div className="input-with-icon">
+        <BsPersonCircle className="icon" />
         <input
           value={nouveauNom}
+          onChange={handleChange}
           type="text"
           placeholder="Entrez votre prénom"
-          onChange={handleChange}
           required
         />
-      </label>
+      </div>
       <button label="Accédez à votre espace">Accéder à mon espace</button>
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.form`
-  background: green;
+  /* background: green; */
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  hr {
+    border: 1.5px solid #ffa51b;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: #ffffff;
+    font-size: 48px;
+  }
+
+  h2 {
+    color: #ffffff;
+    font-size: 36px;
+  }
+
+  .input-with-icon {
+    /* border: 3px solid red; */
+    background-color: #ffffff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      color: #a7a8ad;
+      font-size: 15px;
+      margin-right: 12px;
+    }
+
+    input {
+      border: none;
+      color: "#17161a";
+      font-size: 15px;
+    }
+
+    &::placeholder {
+      background-color: #ffffff;
+      color: lightgrey;
+    }
+  }
 `;
