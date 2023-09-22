2835a926e52d535df768f9bc4b7f2d98824c049e
YohannMaiVan <yohanm888@gmail.com>
2023-09-22 16:36:45 +0200

    💄 F04: ORDER > Add styling

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 import NavbarRightSide from "./NavbarRightSide";
 import Logo from "../../reusable-ui/Logo.jsx";
+import { theme } from "../../../theme";
 
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
       <Logo />
-      <div className="left-side">LEFT</div>
       <NavbarRightSide username={username} />
     </NavbarStyled>
   );
 }
 const NavbarStyled = styled.nav`
-  background: blue;
+  background: ${theme.colors.white};
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
   height: 10vh;
   display: flex;
   justify-content: space-between;
