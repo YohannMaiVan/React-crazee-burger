b216202b87c1ad0eb2bb4db6d93f7e550508fa8e
YohannMaiVan <yohanm888@gmail.com>
2023-09-22 17:02:47 +0200

    💄 F04: ORDER > add primary to background

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -22,13 +22,12 @@ export default function OrderPage() {
 }
 
 const OrderPageStyled = styled.div`
-  background: orange;
+  background: ${theme.colors.primary};
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   .container {
-    background: red;
     height: 95vh;
     width: 1400px;
     display: flex;
