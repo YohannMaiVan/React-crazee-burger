59948d1ad87e56a64d3290f3699c3f0eed0da704
YohannMaiVan <yohanm888@gmail.com>
2023-09-26 13:46:38 +0200

    ♻️ F05: ORDER > create Main and Navbar + docs folders

--- src/components/pages/order/Navbar/Navbar.jsx
+++ src/components/pages/order/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import NavbarRightSide from "./NavbarRightSide";
-import Logo from "../../reusable-ui/Logo.jsx";
-import { theme } from "../../../theme";
-import { refreshPage } from "../../../utils/window";
+import { refreshPage } from "../../../../utils/window";
+import Logo from "../../../reusable-ui/Logo";
+import { theme } from "../../../../theme";
 
 export default function Navbar({ username }) {
   return (
