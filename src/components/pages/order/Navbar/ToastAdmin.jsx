98a76fc9f6da90bcd612b1cc8f5897d0008ae5f7
YohannMaiVan <yohanm888@gmail.com>
2023-09-28 18:25:32 +0200

    ♻️ F06: NAVBAR > Right side > Extract ToastAdmin

--- src/components/pages/order/Navbar/ToastAdmin.jsx
+++ src/components/pages/order/Navbar/ToastAdmin.jsx
@@ -1,17 +1,17 @@
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { theme } from "../../../../theme";
-import { ToastContainer } from "react-toastify";
 import styled from "styled-components";
 
-export default function NavbarRightSideIncomplet() {
+export default function ToastAdmin() {
   return (
-    <NavbarRightSideIncompletStyled>
+    <ToastAdminStyled>
       <ToastContainer className="toaster" bodyClassName="body-toast" />
-    </NavbarRightSideIncompletStyled>
+    </ToastAdminStyled>
   );
 }
 
-const NavbarRightSideIncompletStyled = styled.div`
+const ToastAdminStyled = styled.div`
   .toaster {
     max-width: 300px;
   }
