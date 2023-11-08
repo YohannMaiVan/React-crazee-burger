c26eb73fd14d1441c240753b3359c4a2e544688e
YohannMaiVan <yohanm888@gmail.com>
2023-11-08 16:16:59 +0100

    ♻️ F10 : BASKET > reusable > Create reusable component Header

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components"
 import { theme } from "../../../../../theme"
+import Header from "../../../../reusable-ui/Header"
 
 export default function Basket() {
   return (
     <BasketStyled>
-      <div className="head">Head</div>
+      <Header>Head</Header>
       <div className="body">Body</div>
-      <div className="footer">Footer</div>
+      <Header>Footer</Header>
     </BasketStyled>
   )
 }
@@ -17,19 +18,9 @@ const BasketStyled = styled.div`
   display: flex;
   flex-direction: column;
 
-  .head {
-    background: red;
-    height: 70px;
-    background: ${theme.colors.background_dark};
-  }
   .body {
     flex: 1;
     background: ${theme.colors.background_white};
     box-shadow: ${theme.shadows.basket};
-  }
-  .footer {
-    background: green;
-    height: 70px;
-    background: ${theme.colors.background_dark};
   }
 `
